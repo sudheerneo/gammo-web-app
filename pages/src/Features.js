2d30075cf8b896bb150c22be4e03b0b762f6f156
sudheerneo <52388913+sudheerneo@users.noreply.github.com>
2021-07-21 00:21:19 +0530

    Add files via upload

--- pages/src/Features.js
+++ pages/src/Features.js
@@ -15,7 +15,7 @@ export default function Features() {
 								<a href="#">
 									<div className="icon"> <i className="fa fa-wechat"></i> </div>
 									<h4 className="info-title">Prayer</h4> </a>
-								<p>IF YOU SEE ANY BROTHER OR SISTER COMMIT A SIN THAT DOES NOT LEAD TO DEATH, YOU SHOULD PRAY AND GOD WILL GIVE THEM LIFE. I REFER TO THOSE WHOSE SIN DOES NOT LEAD TO DEATH. THERE IS A SIN THAT LEADS TO DEATH. I AM NOT SAYING THAT YOU SHOULD PRAY ABOUT THAT. 1 JOHN 5:16</p>
+								<p className="text-lowercase">IF YOU SEE ANY BROTHER OR SISTER COMMIT A SIN THAT DOES NOT LEAD TO DEATH, YOU SHOULD PRAY AND GOD WILL GIVE THEM LIFE. I REFER TO THOSE WHOSE SIN DOES NOT LEAD TO DEATH. THERE IS A SIN THAT LEADS TO DEATH. I AM NOT SAYING THAT YOU SHOULD PRAY ABOUT THAT. 1 JOHN 5:16</p>
 							</div>
 						</div>
 						<div className="col-md-4 customizer_repeater_56d7ea7f40b66">
@@ -23,7 +23,7 @@ export default function Features() {
 								<a href="#">
 									<div className="icon"> <i className="fa fa-check"></i> </div>
 									<h4 className="info-title">Healing</h4> </a>
-								<p>HE SAID, “IF YOU LISTEN CAREFULLY TO THE LORD YOUR GOD AND DO WHAT IS RIGHT IN HIS EYES, IF YOU PAY ATTENTION TO HIS COMMANDS AND KEEP ALL HIS DECREES, I WILL NOT BRING ON YOU ANY OF THE DISEASES I BROUGHT ON THE EGYPTIANS, FOR I AM THE LORD, WHO HEALS YOU.” EXODUS 15:26</p>
+								<p className="text-lowercase">HE SAID, “IF YOU LISTEN CAREFULLY TO THE LORD YOUR GOD AND DO WHAT IS RIGHT IN HIS EYES, IF YOU PAY ATTENTION TO HIS COMMANDS AND KEEP ALL HIS DECREES, I WILL NOT BRING ON YOU ANY OF THE DISEASES I BROUGHT ON THE EGYPTIANS, FOR I AM THE LORD, WHO HEALS YOU.” EXODUS 15:26</p>
 							</div>
 						</div>
 						<div className="col-md-4 customizer_repeater_56d7ea7f40b86">
@@ -31,7 +31,7 @@ export default function Features() {
 								<a href="#">
 									<div className="icon"> <i className="fa fa-support"></i> </div>
 									<h4 className="info-title">Support</h4> </a>
-								<p>BUT THOSE WHO HOPE IN THE LORDWILL RENEW THEIR STRENGTH. THEY WILL SOAR ON WINGS LIKE EAGLES; THEY WILL RUN AND NOT GROW WEARY, THEY WILL WALK AND NOT BE FAINT. ISAIAH 40:31</p>
+								<p className="text-lowercase">BUT THOSE WHO HOPE IN THE LORDWILL RENEW THEIR STRENGTH. THEY WILL SOAR ON WINGS LIKE EAGLES; THEY WILL RUN AND NOT GROW WEARY, THEY WILL WALK AND NOT BE FAINT. ISAIAH 40:31</p>
 							</div>
 						</div>
 					</div>
