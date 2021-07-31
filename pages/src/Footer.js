de0d71f689823281e4c449ef1f92d409fa76ff12
sudheerneo <52388913+sudheerneo@users.noreply.github.com>
2021-08-01 04:40:10 +0530

    Update Footer.js

--- pages/src/Footer.js
+++ pages/src/Footer.js
@@ -6,13 +6,14 @@ export default function Footer(){
 <footer className="footer footer-black footer-big">
 				<div className="container">
 					<div className="copyright pull-left"> 
+	    				We are on 
 					<Link href="https://www.facebook.com/Jesus-Loves-you-Ministries-JLUM-235036396947583/">
 							<a> <i className="_mi _before dashicons dashicons-facebook" aria-hidden="true" style={{color: 'lightblue'}}> </i></a>
 						</Link> and 
 						<Link href="https://www.youtube.com/channel/UC26_CXwwmhackoB_H8hjtPQ">
 							<a> <i className="_mi _before dashicons dashicons-youtube" aria-hidden="true" style={{color: 'red', fontSize:24}}> </i> </a>
 						</Link> 
-						&nbsp; follow us on social platforms						 
+						&nbsp; please follow us						 
 					</div>
 				
 					<div className="copyright pull-right"> <Link href="/"><a>Jesus Loves You Ministries</a></Link> | <span data-toggle="tooltip" title='Hello I am Sudheer, Fullstack Web App Developmement expert. Follow us to get your awesome projects like this - Best Regards'><a href="http://sudheerinfo.com/" > Made with <i className="_mi _before dashicons dashicons-heart"  style={{color: 'green'}}></i></a> </span></div>
