2cbafe97c882089f1944357483146257849ea57b
sudheerneo <52388913+sudheerneo@users.noreply.github.com>
2021-08-02 19:53:37 +0530

    Update Footer.js

--- pages/src/Footer.js
+++ pages/src/Footer.js
@@ -5,7 +5,7 @@ export default function Footer(){
 
 <footer className="footer footer-black footer-big" >
 				<div className="container">
-					<div className="copyright pull-left" style={{ fontSize: 10 }} > 
+					<div className="copyright pull-left" style={{ fontSize: 12 }} > 
 	    				We are on 
 					<Link href="https://www.facebook.com/Jesus-Loves-you-Ministries-JLUM-235036396947583/">
 							<a> <i className="_mi _before dashicons dashicons-facebook" aria-hidden="true" style={{color: 'lightblue'}}> </i></a>
@@ -16,7 +16,7 @@ export default function Footer(){
 						&nbsp; please like us, follow us			 
 					</div>
 				
-					<div className="copyright pull-right" style={{ fontSize: 10 }} > <Link href="/"><a>2011 - {new Date().getFullYear()} © Jesus Loves You Ministries</a></Link> | <span data-toggle="tooltip" title='Hello I am Sudheer, Fullstack Web App Developmement expert. Follow us to get your awesome projects like this - Best Regards'><a href="http://sudheerinfo.com/" > Made with <i className="_mi _before dashicons dashicons-heart"  style={{color: 'green'}}></i></a> </span></div>
+					<div className="copyright pull-right" style={{ fontSize: 12 }} > <Link href="/"><a>2011 - {new Date().getFullYear()} © Jesus Loves You Ministries</a></Link> | <span data-toggle="tooltip" title='Hello I am Sudheer, Fullstack Web App Developmement expert. Follow us to get your awesome projects like this - Best Regards'><a href="http://sudheerinfo.com/" > Made with <i className="_mi _before dashicons dashicons-heart"  style={{color: 'green'}}></i></a> </span></div>
 				</div>
 			</footer>
 <script type="text/javascript" src="/srcData_files/windowdom.js"></script>
