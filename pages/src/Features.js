efce838866ed6e0c735439825cb7352553aff2ce
sudheerneo <52388913+sudheerneo@users.noreply.github.com>
2021-08-03 13:31:27 +0530

    Add files via upload

--- pages/src/Features.js
+++ pages/src/Features.js
@@ -6,7 +6,7 @@ export default function Features() {
 				<div className="container">
 					<div className="row">
 						<div className="col-md-8 col-md-offset-2">
-							<h2 className="title">Why our JLUM ministries is so blessed</h2>
+							<h2 className="title">Why our ministries is so blessed</h2>
 							<h5 className="description">In our events you&#039;ll find vibrant, contemporary followers who love the Lord Jesus Christ and express that love in lively praise and worship and in caring relationships.</h5> </div>
 					</div>
 					<div className="row">
--- pages/src/Intro.js
+++ pages/src/Intro.js
@@ -29,15 +29,15 @@ useEffect(()=>{
 										<div className="so-widget-sow-editor so-widget-sow-editor-base">
 											<div className="siteorigin-widget-tinymce textwidget">
 												<p><img className=" wp-image-308 alignleft" src='/images/intro.jpg' alt="" width="657" height="481" style={{borderRadius: 50}} /></p>
-												<p style={{textAlign: "center"}}><strong>Introduction</strong></p>
+												<h2 style={{textAlign: "center", paddingTop : isMobile ? 10 : 20 }}><strong>Introduction</strong></h2>
 												<p style={{textAlign: "center"}}> </p>
-												<p style={{textAlign: isMobile ? 'justify' : 'center' }}>Bro. <strong>D.Vijay Rama Rao</strong> was a young man who started JLUM journey when he was in the city of Hyderabad in India from last 10 plus years. Vijay Ramarao was probably the person who led servant to the Lord (I Tim. 1:2; II Tim. 1:2; I Cor. 4:17), In his life journey he visited the churches around india and ministered there for some time. God can do miracles in our lives in the same manner god made him as his servant. He was very happy with his responsibilities </p>
+												<p style={{textAlign: 'justify'}}>Bro. <strong>D.Vijay Rama Rao</strong> was a young man who started JLUM journey when he was in the city of Hyderabad in India from last 10 plus years. Vijay Ramarao was probably the person who led servant to the Lord (I Tim. 1:2; II Tim. 1:2; I Cor. 4:17), In his life journey he visited the churches around india and ministered there for some time. God can do miracles in our lives in the same manner god made him as his servant. He was very happy with his responsibilities </p>
 												
 												<Link href='/about'>
-                                                  <p style={{textAlign: "center", paddingTop: isMobile ? 2 : 50 }}><strong>want to know more about</strong></p>
+                                                  <p style={{ cursor: 'pointer', textAlign: "center", paddingTop: isMobile ?  30 : 50 }}><strong>want to know more about</strong></p>
 												</Link>
 												<Link href='/about'>
-												  <p style={{textAlign: "center"}}><strong>Bro D. Vijay Rama Rao</strong></p>
+												  <p style={{ cursor: 'pointer', textAlign: "center"}}><strong>Bro D. Vijay Rama Rao</strong></p>
                                               	</Link>	
 
 											</div>
