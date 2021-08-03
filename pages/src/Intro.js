d37add2622ff1371cbcffd2914981a473261ce9e
sudheerneo <52388913+sudheerneo@users.noreply.github.com>
2021-08-03 13:02:34 +0530

    Add files via upload

--- pages/src/Intro.js
+++ pages/src/Intro.js
@@ -8,7 +8,7 @@ const [ isMobile, setMobile ] = useState(false)
 useEffect(()=>{
     const fetchData = async () => {
       const winSize = await window.innerWidth;
-      winSize < 764 ? this.setState({ isMobile: true}) : this.setState({ isMobile: false})
+      winSize < 764 ? setMobile(true) : setMobile(false)
 
     }
     fetchData()
@@ -32,15 +32,14 @@ useEffect(()=>{
 												<p style={{textAlign: "center"}}><strong>Introduction</strong></p>
 												<p style={{textAlign: "center"}}> </p>
 												<p style={{textAlign: isMobile ? 'justify' : 'center' }}>Bro. <strong>D.Vijay Rama Rao</strong> was a young man who started JLUM journey when he was in the city of Hyderabad in India from last 10 plus years. Vijay Ramarao was probably the person who led servant to the Lord (I Tim. 1:2; II Tim. 1:2; I Cor. 4:17), In his life journey he visited the churches around india and ministered there for some time. God can do miracles in our lives in the same manner god made him as his servant. He was very happy with his responsibilities </p>
-												<p style={{textAlign: "center"}}> </p>
+												
 												<Link href='/about'>
-                                                                                                  <p style={{textAlign: "center"}}><strong>know more about</strong></p></Link><Link href='/about'>
+                                                  <p style={{textAlign: "center", paddingTop: isMobile ? 2 : 50 }}><strong>want to know more about</strong></p>
+												</Link>
+												<Link href='/about'>
 												  <p style={{textAlign: "center"}}><strong>Bro D. Vijay Rama Rao</strong></p>
-                                                                                                </Link>
-												<p style={{textAlign: "center"}}>
-													<a className="ow-button-hover" href="/"></a></p>
-												<p>													
-												</p>
+                                              	</Link>	
+
 											</div>
 										</div>
 									</div>
