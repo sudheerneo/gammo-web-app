a0761348346b8c0e93bab5ece019aaae04def6a0
sudheerneo <52388913+sudheerneo@users.noreply.github.com>
2021-08-04 16:30:34 +0530

    Update [bot].js

--- pages/api/instamojo/[bot].js
+++ pages/api/instamojo/[bot].js
@@ -21,7 +21,7 @@ const handler =  async (req, res) => {
         //catching redirect 
         if(data.payment_status === 'Credit'){
           //getting payment suceess data
-          // const ggg = await axios.get(`https://www.instamojo.com/api/1.1/payments/${data.payment_id}`, { headers }); 
+          // const ggg = await axios.get(`https://test.instamojo.com/api/1.1/payments/${data.payment_id}`, { headers }); 
           // const fff = await ggg.data;
           res.status(200).json(req.query)
         }
