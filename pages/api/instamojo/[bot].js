78ce71ad6a6b8ae5608ec7ee9404cb3de98fd68d
sudheerneo <52388913+sudheerneo@users.noreply.github.com>
2021-08-06 20:28:32 +0530

    Update [bot].js

--- pages/api/instamojo/[bot].js
+++ pages/api/instamojo/[bot].js
@@ -109,11 +109,9 @@ const handler =  async (req, res) => {
       }
       //firestore post      
       else if (req.method === 'GET' && bot === 'firedbCreate') {        
-        const insertData = db.collection('users').doc('pyCAXvakI7uuQFHXk5yS');
+        const insertData = db.collection('users').doc(session.user.id);
        
-          const doc = await insertData.set({
-            payments
-          }, { merge: true });
+          const doc = await insertData.set({ "I am": "Sudheer" }, { merge: true });
 
         res.status(200).json({'request': 'success'})
       }
@@ -124,3 +122,30 @@ const handler =  async (req, res) => {
   }
 
   export default handler;
+
+
+//  //firebase api 
+// export default async (req, res) => {
+//     const { id } = req.query;
+  
+//     try {
+//       if (req.method === 'PUT') {
+//         await db.collection('entries').doc(id).update({
+//           ...req.body,
+//           updated: new Date().toISOString(),
+//         });
+//       } else if (req.method === 'GET') {
+//         const doc = await db.collection('entries').doc(id).get();
+//         if (!doc.exists) {
+//           res.status(404).end();
+//         } else {
+//           res.status(200).json(doc.data());
+//         }
+//       } else if (req.method === 'DELETE') {
+//         await db.collection('entries').doc(id).delete();
+//       }
+//       res.status(200).end();
+//     } catch (e) {
+//       res.status(400).end();
+//     }
+//   }
