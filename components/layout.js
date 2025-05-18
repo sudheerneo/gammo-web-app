dc82c39ed3cd6d1c6e68d8c03c15362861e44925
sudheerneo <neosudheer@gmail.com>
2025-05-18 15:44:21 +0530

    fixed dynamic navbar

--- components/layout.js
+++ components/layout.js
@@ -1,5 +1,8 @@
-import Header from "../components/header";
+// import Header from "../components/header";
 import Footer from "../components/footer";
+
+import dynamic from "next/dynamic";
+const Header = dynamic(() => import("../components/header"), { ssr: false }); // <== key fix
 
 export default function Layout({ children }) {
   return (
